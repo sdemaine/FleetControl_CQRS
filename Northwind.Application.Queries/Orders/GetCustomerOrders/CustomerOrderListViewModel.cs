b3a373def09bdbec1baf61592f50d5edfd6fdb02
Stephen Demaine <stephendemaine@live.com>
2019-08-16 20:27:43 -0400

    Initial commit

---
+++ Northwind.Application.Queries/Orders/GetCustomerOrders/CustomerOrderListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Northwind.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Application.Queries.Orders.GetCustomerOrders
+{
+    public class CustomerOrderListViewModel
+    {
+        public CustomerOrderListViewModel()
+        {
+            CustomerOrders = new List<OrderDto>();
+        }
+        public IEnumerable<OrderDto> CustomerOrders { get; set; }
+
+        public bool CreateEnabled { get; set; }
+    }
+}
