95f98b633a0c479c24f535abf801028f47970f1d
Stephen Demaine <stephendemaine@live.com>
2019-08-20 00:21:38 -0400

    removed Northwind commands folder

--- FleetControl.Application.Commands/Northwind/Customers/DeleteCustomer/DeleteCustomerCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace Northwind.Application.Commands.DeleteCustomer
-{
-    public class DeleteNorthwindCustomerCommand : IRequest
-    {
-        public string CustomerId { get; set; }
-    }
-}
--- FleetControl.Application.Commands/Northwind/Products/DeleteProduct/DeleteNorthwindProductCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace Northwind.Application.Commands.DeleteProduct
-{
-    public class DeleteNorthwindProductCommand : IRequest
-    {
-        public int Id { get; set; }
-    }
-}
